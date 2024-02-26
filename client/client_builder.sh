107a17a111bb0579e9d832f72f14b9ffd16106d5
SXY <shen13515251251@gmail.com>
2024-02-26 22:19:43 +0800

    123

--- client/client_builder.sh
+++ client/client_builder.sh
@@ -1,4 +1,4 @@
-docker build -t xs90_cli:0.4 .
+docker build -t xs90_cli:0.5 .
 # docker build --no-cache -t xs90_cli:0.1 .
-docker tag xs90_cli:0.4 xingyushen/xs90_cli:0.4
-docker push xingyushen/xs90_cli:0.4
+docker tag xs90_cli:0.5 xingyushen/xs90_cli:0.5
+docker push xingyushen/xs90_cli:0.5
